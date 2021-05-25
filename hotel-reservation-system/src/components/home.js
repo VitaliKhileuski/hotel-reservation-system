56ee642534eeb84addc32d283aa9544201063bbe
Vitali Khileuski <kentkentkent28@gmail.com>
2021-05-26 02:49:58 +0300

    added country and city combo boxes

--- hotel-reservation-system/src/components/home.js
+++ hotel-reservation-system/src/components/home.js
@@ -6,7 +6,7 @@ import { Typography , Button} from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios'
+import API from './../api'
 
 
 
@@ -17,26 +17,55 @@ const useStyles = makeStyles({
       marginRight: 10,
       fontSize: 18,
     },
+    grid : {
+      MarginTop : 30
+    }
   },
-  comboBox: {
-    MarginTop : 15
-  }
 });
 
 export default function Home(){
+  const [city,setCity] = useState('');
   const [countries,setCountries] = useState([]);
+  const [cities,setCities] = useState([]);
+  const [currentCountry, setCurrentCountry] = useState("");
+
   useEffect(() => {
     const loadCountries = async () => {
-      await  axios
-      .get('https://restcountries.eu/rest/v2/all')
+      await  API
+      .get('/locations/countries')
       .then(response => response.data)
-      .then((data) => setCountries(data))
+      .then((data) => {
+        setCountries(data);
+        console.log(data);
+      })
       .catch((error) => console.log(error));
+
     };
     loadCountries();
+    
   }, []);
+    useEffect(() => {
+      const loadCities = async () => {
+        await  API
+        .get('/locations/cities/'+currentCountry)
+        .then(response => response.data)
+        .then((data) => {
+          setCities(data);
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+  
+      };
+      console.log(cities);
+      setCities([]);
+      console.log(cities);
+      if(currentCountry!==""){
+        loadCities();
+      }
+      
+    },[currentCountry])
 
-  const classes = useStyles();
+    const classes = useStyles();
     const [checkInDate, setCheckInDate] = useState(Date.now);
     const [checkOutDate, setCheckOutDate] = useState(new Date(Date.now() + 24*60*60*1000));
     const tommorow  = new Date(Date.now() + 24*60*60*1000);
@@ -56,15 +85,15 @@ export default function Home(){
           >
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <Grid>
-            <Typography variant='h6' margin='normal'>
+            <Typography variant='h6'>
               Check in Date
             </Typography>
             <KeyboardDatePicker
               disableToolbar
               disablePast
               variant="inline"
+              inputVariant="outlined"
               format="MM/dd/yyyy"
-              margin="normal"
               value={checkInDate}
               onChange={handleDateCheckInChange}
               KeyboardButtonProps={{
@@ -73,15 +102,15 @@ export default function Home(){
             />
           </Grid>
           <Grid>
-          <Typography variant='h6' margin='normal'>
+          <Typography variant='h6'>
               Check out date
             </Typography>
             <KeyboardDatePicker
               disableToolbar
               minDate={tommorow}
               variant="inline"
-              margin="normal"
               format="MM/dd/yyyy"
+              inputVariant="outlined"
               value={checkOutDate}
               onChange={handleDateCheckOutChange}
               KeyboardButtonProps={{
@@ -91,18 +120,26 @@ export default function Home(){
           </Grid>
             </MuiPickersUtilsProvider>
           <Grid>
-            <Typography variant = 'h6' margin='nornal'>
+            <Typography variant = 'h6'>
               What place do you want to visit?
             </Typography>
                 <Autocomplete 
                 id="combo-box-demo"
                 options={countries}
-                margin="normal"
-                getOptionLabel={(option) => option.name}
+                onChange={(event, value) => setCurrentCountry(value)}
+                getOptionLabel={(option) => option}
                 style={{ width: 300 }}
                 renderInput={(params) => <TextField  {...params} label="choose your counrty" variant="outlined" />}
                 />
           </Grid>
+          <Grid> <Autocomplete 
+                id="combo-box-demo2"
+                options={cities}
+                getOptionLabel={(option) => option}
+                style={{ width: 300 }}
+                value = {city}
+                renderInput={(params) => <TextField  {...params} label="choose your city" variant="outlined" />}
+                /></Grid>
           <Grid>
             <Button variant="contained" color="primary" size='large'>
              Search
